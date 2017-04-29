6066b559a406d9f1bcca19adb04bbc7512eda19d
Camilo Polymeris <cpolymeris@gmail.com>
2017-04-29 15:18:43 -0300

    Remove .idea, should never have been commited

---
+++ src/cljs/conduit/views/core.cljs
@@ -0,0 +1,33 @@
+(ns conduit.views.core
+  (:require [re-frame.core :as re-frame]))
+
+(defn home-panel []
+  (let [name (re-frame/subscribe [:name])]
+    (fn []
+      [:div (str "Hello from " @name ". This is the Home Page.")
+       [:div [:a {:href "#/about"} "go to About Page"]]])))
+
+
+;; about
+
+(defn about-panel []
+  (fn []
+    [:div "This is the About Page."
+     [:div [:a {:href "#/"} "go to Home Page"]]]))
+
+
+;; main
+
+(defn- panels [panel-name]
+  (case panel-name
+    :home-panel [home-panel]
+    :about-panel [about-panel]
+    [:div]))
+
+(defn show-panel [panel-name]
+  [panels panel-name])
+
+(defn main-panel []
+  (let [active-panel (re-frame/subscribe [:active-page])]
+    (fn []
+      [show-panel @active-panel])))
