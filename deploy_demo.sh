fe7e84dfdc63d45c7cc480fc5c6be5328e68eb98
Camilo Polymeris <cpolymeris@gmail.com>
2017-05-01 02:17:43 -0300

    Set author in deploy script

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -4,7 +4,8 @@ lein cljsbuild once min
 git clone git@github.com:polymeris/re-frame-realword-example-app-demo.git demo
 cd demo
 cp -R ../resources/public/* .
-git commit --allow-empty -am 'Update'
+git config user.email '<deploy@conduit>'
+git commit --allow-empty 'Update'
 git push
 cd ..
 rm -Rf demo
