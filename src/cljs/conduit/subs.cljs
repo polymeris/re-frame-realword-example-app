1015ac9fe7aa29de2162a58e9e754050689e87d1
Camilo Polymeris <cpolymeris@gmail.com>
2017-05-02 01:53:27 -0300

    Request pending and errors subscription

--- src/cljs/conduit/subs.cljs
+++ src/cljs/conduit/subs.cljs
@@ -8,11 +8,24 @@
     (:active-page db)))
 
 (reg-sub
-  :pending-requests
-  (fn [db query]
-    (get-in db query)))
+  :request-errors
+  (fn [db [_ & query]]
+    (let [status (get-in db (into [:pending-requests] query))]
+      (when (and (vector? status)
+                 (= :failed (first status)))
+        (second status)))))
+
+(reg-sub
+  :request-pending
+  (fn [db [_ & query]]
+    (= :pending (get-in db (into [:pending-requests] query)))))
 
 (reg-sub
   :user
   (fn [db _]
     (:user db)))
+
+(reg-sub
+  :tags
+  (fn [db _]
+    (:tags db)))
