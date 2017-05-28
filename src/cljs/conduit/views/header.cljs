558167fa37c0f2e85d61196a024a740dfcb3d822
Camilo <camilo@wivo.cl>
2017-05-28 06:19:10 -0400

    Show user avatar in header

--- src/cljs/conduit/views/header.cljs
+++ src/cljs/conduit/views/header.cljs
@@ -11,7 +11,8 @@
       text]]))
 
 (defn navbar []
-  (let [user (re-frame/subscribe [:user])]
+  (let [user (re-frame/subscribe [:user])
+        active-page (re-frame/subscribe [:active-page])]
     [:nav.navbar.navbar-light
      [:a.navbar-brand {:href "#/"} "conduit"]
      (if @user
@@ -19,7 +20,11 @@
         [navitem {:text "Home" :target :home}]
         [navitem {:icon-class :ion-compose :text "New Post" :target :editor}]
         [navitem {:icon-class :ion-gear-a :text "Settings" :target :settings}]
-        [navitem {:text (:username @user) :target (str "profile/" (:username @user))}]
+        [:li.nav-item
+         [:a.nav-link {:class (when (= @active-page :profile) "active")
+                       :href  (str "#/profile/" (:username @user))}
+          [:img.user-pic {:src (:image @user)}]
+          (:username @user)]]
         [navitem {:text "Log out" :target :logout}]]
        [:ul.nav.navbar-nav.pull-xs-right
         [navitem {:text "Log in" :target :login}]
